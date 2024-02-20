5b0f8f8dff5e9b428014907708ac4e2219e07d7a
VladlmlrF <vladimirfalin84@gmail.com>
2024-02-20 16:33:39 +0700

    add clients_router

--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
 import uvicorn
 from fastapi import FastAPI
 
+from src.app.clients.views import router as clients_router
+
 app = FastAPI(title="Mailing manager")
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello"}
+
+
+app.include_router(clients_router, prefix="/clients")
 
 
 if __name__ == "__main__":
